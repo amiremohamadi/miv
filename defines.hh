c06f26f6315cf91b2c7011cef275f8f47b93fd95
amir <amiremohamadi@yahoo.com>
2019-11-05 22:13:11 +0330

    add color pairs, splash message and version define

--- defines.hh
+++ defines.hh
@@ -2,6 +2,9 @@
 #define DEFINE_HH_
 
 #include <iostream>
+#include <string>
+
+#define VERSION "0.0.1"
 
 #define CTRL_KEY(key) (key & 0x1F) // 00011111 -> ctrl is pressed
 
@@ -12,5 +15,18 @@ do { \
     std::cerr << msg; \
     exit(1); \
 } while (false) \
+
+static std::string messages[] = {
+    "MIV -- easier than vim",
+    "VERSION " VERSION,
+    "by @amiremohamadi"
+};
+
+// enum index for color pairs
+enum ColorPairs {
+    BLACK_WHITE = 0,
+    BLACK_BLUE,
+    BLACK_RED,
+};
 
 #endif // DEFINES_H_
