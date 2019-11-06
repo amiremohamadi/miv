f5b6e7ebd2bf00e67ccf66379bf912b2018dd8a7
amir <amiremohamadi@yahoo.com>
2019-11-06 22:14:23 +0330

    add open file support

--- src/file.cc
+++ src/file.cc
@@ -3,9 +3,32 @@
 File::File() : file() {}
 
 File::File(std::string file) {
-    this->file = file;
+  this->file = file;
+}
+
+void File::open() {
+  if (this->file.empty())
+    ERROR_HANDLER("can't open file (name is empty)");
+
+  std::ifstream input(file);
+
+  std::string line;
+
+  while (std::getline(input, line)) this->lines.push_back(line);
+}
+
+void File::set_file(std::string file) {
+  this->file = file;
+}
+
+std::string File::get(size_t indx) {
+  return this->lines[indx];
+}
+
+size_t File::size() {
+  return this->lines.size();
 }
 
 bool File::is_empty() {
-    return this->file.empty();
+  return this->file.empty();
 }
--- src/file.hh
+++ src/file.hh
@@ -2,15 +2,23 @@
 #define FILE_HH_
 
 #include <string>
+#include <fstream>
+#include <vector>
+#include "defines.hh"
 
 class File {
   private:
     std::string file;
+    std::vector<std::string> lines;
 
   public:
     File();
     File(std::string);
 
+    void open();
+    void set_file(std::string);
+    std::string get(size_t);
+    size_t size();
     bool is_empty();
 };
 
